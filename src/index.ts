ee8fd25aab34904779351f9aea55c62ab8bfe414
RichardTry <bazrovstepan@gmail.com>
2024-06-08 15:48:52 +0300

    index.ts clearify

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { configDotenv } from "dotenv";
 import { sleep, sendBatch } from "./helpers";
-import {HighloadQueryId} from "./HighloadQueryId";
-import {address, StateInit, Address, toNano, SendMode, storeMessageRelaxed, internal, OutActionSendMsg} from "@ton/core";
-import {mnemonicToWalletKey, KeyPair, sign} from "@ton/crypto";
-import {Slice, Dictionary, beginCell, Cell, TonClient} from "@ton/ton";
-import {readFile, readFileSync} from "fs";
-import crypto from "crypto";
+import { HighloadQueryId } from "./HighloadQueryId";
+import { address, Address, toNano, SendMode, internal, OutActionSendMsg } from "@ton/core";
+import { mnemonicToWalletKey } from "@ton/crypto";
+import { Dictionary, beginCell, Cell, TonClient } from "@ton/ton";
+import { readFileSync } from "fs";
 
 const getRandom = (min:number, max:number) => {
     return Math.random() * (max - min) + min;
@@ -26,36 +25,6 @@ const maxShift      = maxKeyCount - 1;
 const HIGHLOAD_JETTON_WALLET = address("kQA6eZO4J134_wBqD80tkw6-9pjufOkS1XlvA878YxEbeFtt")
 const HIGHLOAD_ADDRESS = address("0QCm6iMpPU-29lHXjIZDc41J7XHXufeKq3zMUSVqC8OvEWFF")
 const JETTON_DECIMALS: number = 9
-const JETTON_CODE = Cell.fromBase64('te6ccgECEQEAAyMAART/APSkE/S88sgLAQIBYgIDAgLMBAUAG6D2BdqJofQB9IH0gahhAgHUBgcCASAICQDDCDHAJJfBOAB0NMDAXGwlRNfA/AM4PpA+kAx+gAxcdch+gAx+gAwc6m0AALTH4IQD4p+pVIgupUxNFnwCeCCEBeNRRlSILqWMUREA/AK4DWCEFlfB7y6k1nwC+BfBIQP8vCAAET6RDBwuvLhTYAIBIAoLAIPUAQa5D2omh9AH0gfSBqGAJpj8EIC8aijKkQXUEIPe7L7wndCVj5cWLpn5j9ABgJ0CgR5CgCfQEsZ4sA54tmZPaqQB8VA9M/+gD6QCHwAe1E0PoA+kD6QNQwUTahUirHBfLiwSjC//LiwlQ0QnBUIBNUFAPIUAT6AljPFgHPFszJIsjLARL0APQAywDJIPkAcHTIywLKB8v/ydAE+kD0BDH6ACDXScIA8uLEd4AYyMsFUAjPFnD6AhfLaxPMgMAgEgDQ4AnoIQF41FGcjLHxnLP1AH+gIizxZQBs8WJfoCUAPPFslQBcwjkXKRceJQCKgToIIJycOAoBS88uLFBMmAQPsAECPIUAT6AljPFgHPFszJ7VQC9ztRND6APpA+kDUMAjTP/oAUVGgBfpA+kBTW8cFVHNtcFQgE1QUA8hQBPoCWM8WAc8WzMkiyMsBEvQA9ADLAMn5AHB0yMsCygfL/8nQUA3HBRyx8uLDCvoAUaihggiYloBmtgihggiYloCgGKEnlxBJEDg3XwTjDSXXCwGAPEADXO1E0PoA+kD6QNQwB9M/+gD6QDBRUaFSSccF8uLBJ8L/8uLCBYIJMS0AoBa88uLDghB73ZfeyMsfFcs/UAP6AiLPFgHPFslxgBjIywUkzxZw+gLLaszJgED7AEATyFAE+gJYzxYBzxbMye1UgAHBSeaAYoYIQc2LQnMjLH1Iwyz9Y+gJQB88WUAfPFslxgBDIywUkzxZQBvoCFctqFMzJcfsAECQQIwB8wwAjwgCwjiGCENUydttwgBDIywVQCM8WUAT6AhbLahLLHxLLP8ly+wCTNWwh4gPIUAT6AljPFgHPFszJ7VQ=');
-const JETTON_MASTER = address("0:d7769f0958602cce023bd0d6efcda84f35cbe3e49189cd3d984d5711f98f7fd7")
-
-function pack_jetton_wallet_data(balance: number,
-                                 owner_address: Address,
-                                 jetton_master_address: Address,
-                                 jetton_wallet_code: Cell): Cell {
-    return  beginCell()
-            .storeCoins(balance)
-            .storeAddress(owner_address)
-            .storeAddress(jetton_master_address)
-            .storeRef(jetton_wallet_code)
-            .endCell();
- }
-
-//  function calculate_jetton_wallet_address(state_init: Cell): Address {
-//     return address(beginCell().storeUint(4, 3)
-//                        .storeInt(workchain(), 8)
-//                        .storeUint(cell_hash(state_init), 256)
-//                        .endCell())
-// }
-
-function calculate_jetton_wallet_state_init(owner_address: Address, jetton_master_address: Address, jetton_wallet_code: Cell): Cell{
-    return beginCell()
-           .storeUint(0, 2)
-           .storeRef(jetton_wallet_code)
-           .storeRef(pack_jetton_wallet_data(0, owner_address, jetton_master_address, jetton_wallet_code))
-           .storeUint(0, 1)
-           .endCell();
-}
 
 async function main() {
     configDotenv();
@@ -98,12 +67,10 @@ async function main() {
     let i: number = 0;
 
     for (const eligible_addr in eligible_map) {
-        console.log(`${i}: ${eligible_addr}`);
+        console.log(`${i+1}: ${eligible_addr}`);
         let eligible_amount = Math.floor(eligible_map[eligible_addr] * 10**JETTON_DECIMALS)
-        //console.log(`Amount: ${eligible_amount}`)
         let userWallet: Address = address(eligible_addr)
 
-        //console.log('Building transfer body ')
         let transferBody = beginCell()
                 .storeUint(0xf8a7ea5, 32)
                 .storeUint(i, 64)
@@ -114,7 +81,7 @@ async function main() {
                 .storeCoins(0)
                 .storeUint(0, 1)
                 .endCell();
-        //console.log('...built!')
+
         outMsgs[i] = {
             type: 'sendMsg',
             mode: SendMode.NONE,
@@ -126,7 +93,6 @@ async function main() {
         }
         i++;
     }
-    console.log('Packaging finished.');
 
     const rndShift: number   = getRandomInt(0, maxShift);
     const rndBitNum: number  = getRandomInt(0, 1022);
@@ -155,12 +121,7 @@ async function main() {
 (() => {
     main()
         .catch(e => {
-            //console.log(e);
-            // if (JSON.stringify(e).length == 2) {
-            //     bot.api.sendMessage(serviceChatID, `Fatal error: ${e}`);
-            //     return;
-            // }
-            // bot.api.sendMessage(serviceChatID, `Fatal error: ${JSON.stringify(e).slice(0, 300)} `);
+            console.log(e);
         })
         .finally(() => console.log("Exiting..."));
 })()
